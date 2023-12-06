1eca1a23a3591d7098ac15f5a40c9efd095b79f8
SnehaArun1 <sneha.arun1@gmail.com>
2023-12-06 17:11:52 +0530

    changes

--- pages/modal.js
+++ pages/modal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from 'react-bootstrap/Button';
-
+import styles from '../styles/Home.module.css';
 import Modal from 'react-bootstrap/Modal';
 
     export default function  ModalPopup(props) { 
@@ -12,7 +12,7 @@ import Modal from 'react-bootstrap/Modal';
         };
       
       return (
-        <Modal
+        <Modal className={styles.modal_bg}
         {...props}
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
@@ -20,19 +20,25 @@ import Modal from 'react-bootstrap/Modal';
       >
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            Modal heading
+          <h4 className={styles.header_title}>Selected tickets are <span className={styles.banner_title_pink}>saved!</span></h4>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h4>Centered Modal</h4>
-          <p>
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-            consectetur ac, vestibulum at eros.
-          </p>
+        <div className={styles.popup_img_wrap}>
+        <img src='/ticket_popup.png' className={styles.popup_immg} />
+        </div>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={props.onHide}>Close</Button>
+
+          <div className={styles.calendar_btn_wrap}>
+
+<button className={styles.calendar_btn}>View tickets</button>
+
+<button className={styles.ok_btn}>Ok</button>
+
+
+</div>
         </Modal.Footer>
       </Modal>
           );
